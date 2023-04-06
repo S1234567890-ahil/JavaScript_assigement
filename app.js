99c3b8e314cc171651ac68717cf42a5a1b04d6de
Sahil Kumar <110956024+S1234567890-ahil@users.noreply.github.com>
2023-04-07 01:40:53 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,119 @@
+//JavaScript Assigemnt
+
+//----------Question  no: 1-------------------------------
+// let numberOne = prompt("Enter First number")
+// let numberTwo = prompt("Enter second  number")
+// if (numberOne > numberTwo){
+//     console.log(numberOne)
+// }
+// else {
+//     console.log(numberTwo)
+// }
+
+
+//----------Question  no: 2   -------------------------------
+
+// let numberOne = prompt("Enter number to check positive or negative   ")
+// if (numberOne >=0){
+//     alert("The sign is +")
+// }
+// else{
+//     alert("The sign is -")
+// }
+
+//----------Question  no: 3   -------------------------------
+
+// let  num1 = prompt("Enter first number");
+// let num2 = prompt("Enter Second number");
+// let num3 = prompt("Enter Third number");
+// let num4 = prompt("Enter Fourth number");
+// let num5 = prompt("Enter Fifth number");
+
+// let largest = num1;
+// if(num2 > largest){
+//     largest = num2;
+// }
+// if(num3 > largest){
+//     largest = num3;
+// }
+// if(num4 > largest){
+//     largest = num4;
+// }
+// if(num5 > largest){
+//     largest = num5;
+// }
+// console.log("The Largest Number is " + largest)
+
+
+//----------Question  no: 4  -------------------------------
+// for (let i = 0; i <= 15; i++){
+//     if(i%2 ===0) {
+//         document.write(i + " is even."+"<br/>")
+//     }else {
+//         document.write(i+" is odd."+"<br/>")
+//     }
+// }
+
+
+//----------Question  no: 5 -------------------------------
+
+// let marks = prompt("Enter your marks")
+// if(marks < 60){
+//     document.write("Your grade is F")
+// }
+// else if(marks < 70) {
+//     document.write("Your grade is D")
+// }
+// else if(marks < 80) {
+//     document.write("Your grade is C")
+// }
+// else if(marks < 90) {
+//     document.write("Your grade is B")
+// }else{
+//     document.write("Your grade is A")
+// }
+
+//----------Question  no: 6 -------------------------------
+
+// for (let i = 0; i <= 100;  i++) {
+//      if(i % 3 ===0 && i % 5 ===0) {
+//         console.log(i + " FizzBuzz" )
+//     }
+//      else if(i % 3 ===0) {
+//         console.log(i + " Fizz" )
+//     }
+//     else if(i % 5 === 0){
+//         console.log(i+" Buzz" )
+//     }
+    
+//     else{
+//         console.log(i)
+//     }
+// }
+
+
+//----------Question  no: 7 -------------------------------
+
+// let n = 5;
+// for(let i = 1; i <= n ; i++ )
+// {
+//     for(let j = 1; j <= i ;j++)
+//         document.write("*  " );
+//         document.write("<br/>");    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
